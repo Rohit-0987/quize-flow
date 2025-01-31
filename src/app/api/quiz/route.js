378e5c37edf0331914e7ecd49a35142363fae71e
Rohit-0987 <99091666+Rohit-0987@users.noreply.github.com>
2025-01-31 15:16:59 +0530

    remove export

--- src/app/api/quiz/route.js
+++ src/app/api/quiz/route.js
@@ -1,23 +1,22 @@
 export async function GET() {
     try {
-      const response = await fetch('https://api.jsonserve.com/Uw5CrX');
-      if (!response.ok) throw new Error('Failed to fetch quiz data');
-  
-      const data = await response.json();
-  
-      return new Response(JSON.stringify(data), {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+        const response = await fetch('https://api.jsonserve.com/Uw5CrX');
+        if (!response.ok) throw new Error('Failed to fetch quiz data');
+
+        const data = await response.json();
+
+        return new Response(JSON.stringify(data), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     } catch (error) {
-      return new Response(JSON.stringify({ error: 'Failed to fetch quiz data' }), {
-        status: 500,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+        return new Response(JSON.stringify({ error: 'Failed to fetch quiz data' }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     }
-  }
-  
+}
